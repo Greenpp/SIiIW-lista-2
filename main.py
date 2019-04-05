fe030acd178bdd7182bf6380998fff736939a115
Jakub Ciszek <jakubciszek64@gmail.com>
2019-04-05 10:45:40 +0200

    Data loading

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from scp import SCP
 scp = SCP(method='back',
           order='none')
 
-if scp.load_data('train_data/futoshiki_4_0.txt', 'futo'):
+if scp.load_data('train_data/futoshiki_4_0.txt'):
     scp.run()
--- scp.py
+++ scp.py
@@ -29,39 +29,140 @@ class SCP:
         self.constraints = dict()
         self.pointer = 0
 
-    def load_data(self, file_path, type_):
+    def load_data(self, file_path, type_=None):
         """
         Load data from file
 
         :param file_path:   Path to data file
         :param type_:       Type of problem:
+                                        None    - Base on file name
                                         futo    - Futoshiki
                                         sky     - Skyscrapper
 
         :return:    If data was loaded successfully
         """
+        if type_ is None:
+            # Guess data type base on file name
+            file_types = {
+                'futoshiki': 'futo',
+                'skyscrapper': 'sky'
+            }
+            separator = '/' if '/' in file_path else '\\'
+            file_name = file_path.split(separator)[-1].lower()
+            for word, t in file_types.items():
+                if word in file_name:
+                    type_ = t
+                    break
+
+        if type_ == 'futo':
+            return self._load_futoshiki_file(file_path)
+        elif type_ == 'sky':
+            return self._load_skyscrapper_file(file_path)
+        else:
+            print('Wrong file type')
+            return False
+
+    def _load_skyscrapper_file(self, file_path):
+        """
+        Load skyscrapper data from file
+
+        :param file_path:   Path to data file
+
+        :return:    If data was loaded successfully
+        """
         with open(file_path) as f:
-            if type_ == 'futo':
-                n = int(f.readline())  # Read the size
+            # TODO return False when failed
+            n = int(f.readline())  # Read the size
+            default_domain = [v + 1 for v in range(n)]
 
-                f.readline()  # Skip 'START:'
-                for i in range(n):
-                    row = f.readline().rstrip()
-                    # TODO parse row
+            # Tmp structure for constraints definition
+            tmp_table = [[] for i in range(n)]
 
-                f.readline()  # Skip 'REL:'
-                for line in f:
-                    # TODO parse constraints + strip
-                    pass
-            elif type_ == 'sky':
-                n = int(f.readline())  # Read the size
+            # Create variables
+            for i in range(n):
+                for j in range(n):
+                    pos = (i, j)
+                    var = _Variable(pos, default_domain)
 
-                for line in f:
-                    # TODO parse constraints + strip
-                    pass
-            else:
-                print('Wrong file type')
-                return False
+                    tmp_table[i].append(var)
+
+                    self.call_stack.append(var)
+
+            # Load constraints
+            for line in f:
+                line = line.rstrip()
+
+                for side, *vals in line.split(';'):
+                    if side == 'G':
+                        pass
+                    elif side == 'D':
+                        pass
+                    elif side == 'L':
+                        pass
+                    elif side == 'P':
+                        pass
+                    # TODO create rows and columns constraints
+
+        return True
+
+    def _load_futoshiki_file(self, file_path):
+        """
+        Load futoshiki data from file
+
+        :param file_path:   Path to data file
+
+        :return:    If data was loaded successfully
+        """
+        with open(file_path) as f:
+            # TODO return False when failed
+            n = int(f.readline())  # Read the size
+            default_domain = [v + 1 for v in range(n)]
+
+            # Tmp structure for constraints definition
+            tmp_table = [[] for i in range(n)]
+
+            # Load variables
+            f.readline()  # Skip 'START:'
+            for i in range(n):
+                row = f.readline().rstrip()
+                cells = row.split(';')
+
+                # Load variables for row
+                for j, val in enumerate(cells):
+                    val = int(val)
+                    domain = tuple(default_domain) if val == 0 else (val,)
+                    pos = (i, j)
+
+                    var = _Variable(pos, domain)
+                    tmp_table[i].append(var)
+
+                    # Add only mutable variables to stack
+                    if not var.fixed:
+                        self.call_stack.append(var)
+
+            # Load relations
+            f.readline()  # Skip 'REL:'
+            for line in f:
+                line = line.rstrip()
+                cell1, cell2 = line.split(';')
+
+                row1 = ord(cell1[0]) - 65
+                col1 = int(cell1[1:])
+
+                row2 = ord(cell2[0]) - 65
+                col2 = int(cell2[1:])
+
+                var1 = tmp_table[row1][col1]
+                var2 = tmp_table[row2][col2]
+
+                # TODO create constraint var1 < var2
+
+            # Create rows and columns constrains
+            for i in range(n):
+                row_vars = tmp_table[i]
+                col_vars = [tmp_table[r][i] for r in range(n)]
+
+                # TODO create rows and columns constraints
 
         return True
 
