249014d155d77abcbe1c4b1cb554b7d04d282091
Jakub Ciszek <jakubciszek64@gmail.com>
2019-04-05 08:52:01 +0200

    Var prepared for hashing, data loading structure

--- scp.py
+++ scp.py
@@ -9,7 +9,7 @@ class SCP:
         order           Method of ordering call stack:
                                         none    - stack is left in default order
         call_stack      List of variables in filling order
-        constraints     List of problems constraints
+        constraints     Dictionary of variable: list of constraints its included into
         pointer         Index of variable currently changed, solving terminates when out of stack range
     """
 
@@ -26,7 +26,7 @@ class SCP:
         self.method = method
         self.order = order
         self.call_stack = []
-        self.constraints = []
+        self.constraints = dict()
         self.pointer = 0
 
     def load_data(self, file_path, type_):
@@ -42,11 +42,23 @@ class SCP:
         """
         with open(file_path) as f:
             if type_ == 'futo':
-                # TODO load futo data
-                pass
+                n = int(f.readline())  # Read the size
+
+                f.readline()  # Skip 'START:'
+                for i in range(n):
+                    row = f.readline().rstrip()
+                    # TODO parse row
+
+                f.readline()  # Skip 'REL:'
+                for line in f:
+                    # TODO parse constraints + strip
+                    pass
             elif type_ == 'sky':
-                # TODO load sky data
-                pass
+                n = int(f.readline())  # Read the size
+
+                for line in f:
+                    # TODO parse constraints + strip
+                    pass
             else:
                 print('Wrong file type')
                 return False
@@ -87,20 +99,56 @@ class _Variable:
     Single variable in system
 
     Attributes:
+        id_             Unique variable identifier
         domain          Domain of the variable, immutable
         work_domain     Modifiable domain used for value picking
         value           Current value of the variable
+        fixed           If variable value is fixed
     """
 
-    def __init__(self, domain):
+    def __init__(self, id_, domain):
         """
         Create variable with given domain
 
         :param domain: immutable domain
         """
+        self.id_ = id_
         self.domain = domain
         self.work_domain = None
-        self.value = None
+        if len(domain) == 1:
+            self.value = domain[0]
+            self.fixed = True
+        else:
+            self.value = None
+            self.fixed = False
+
+    def __eq__(self, other):
+        """
+        Check if objects are equal
+
+        :param other:   Object to compare to
+
+        :return:    If objects are equal
+        """
+        return self.id_ == other.id_
+
+    def __ne__(self, other):
+        """
+        Check if objects are different
+
+        :param other:   Object to compare to
+
+        :return:    If objects are equal
+        """
+        return not (self == other)
+
+    def __hash__(self):
+        """
+        Get objects hash
+
+        :return: Objects hash
+        """
+        return hash(self.id_)
 
     def reset_domain(self):
         """
