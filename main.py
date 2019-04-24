6ecb1b956de1394cb856f42d31747a8222a952ca
Jakub Ciszek <jakubciszek64@gmail.com>
2019-04-25 00:08:17 +0200

    Skyscrapper execution

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from timeing import timeit
 
 @timeit
 def run():
-    scp = SCP(method='forward',
-              order='min_dom',
-              dynamic_ordering=True,
-              all_solutions=True)
+    scp = SCP(method='back',
+              order='none',
+              dynamic_ordering=False,
+              all_solutions=False)
 
     # if scp.load_data('test_data/test_futo_9_0.txt'):
-    if scp.load_data('train_data/futoshiki_5_4.txt'):
+    # if scp.load_data('train_data/futoshiki_5_4.txt'):
+    # if scp.load_data('train_data/skyscrapper_4_1.txt'):
+    if scp.load_data('test_data/test_sky_6_0.txt'):
         scp.run()
         scp.show_solutions()
 
